2129e11c51e90d1513d79b9ec81e825a4002303b
CangyuanLi <everest229@gmail.com>
2022-04-29 13:27:21 -0400

    algorithm explanation

--- Week 4/cl4220_hw4_q3.cpp
+++ Week 4/cl4220_hw4_q3.cpp
@@ -6,6 +6,15 @@ int main()
     std::cout << "Enter a decimal number:\n";
     std::cin >> base_ten_number;
 
+    /**
+     * The algorithm shown in class is to divide the decimal number by 2, save the remainder, and
+     * use the new quotient in the next iteration. The remainders are then concatenated in 
+     * reverse order, ie. the last iteration is the first digit of the binary number. Since we are
+     * not allowed to use arrays or strings, represent the remainder of the first iteration 
+     * as the ones place, the second iteration as the tens place, and so on. This is accomplished
+     * by multiplying by 1, 10, 100, 1000, and so on.
+     */
+
     int binary_result = 0;
     int digits_place = 1;
     int quotient = base_ten_number;
