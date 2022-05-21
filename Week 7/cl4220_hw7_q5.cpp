6a7f406529de2ba27d94456b47f5414022ef0411
CangyuanLi <everest229@gmail.com>
2022-05-20 20:59:44 -0400

    comments + formatting

--- Week 7/cl4220_hw7_q5.cpp
+++ Week 7/cl4220_hw7_q5.cpp
@@ -36,7 +36,7 @@ int *get_pos_nums1(int *arr, int arr_size, int &out_pos_arr_size)
 
 int *get_pos_nums2(int *arr, int arr_size, int *out_pos_arr_size_ptr)
 {
-    *out_pos_arr_size_ptr = 0;
+    *out_pos_arr_size_ptr = 0; // dereference the pointer
     for (int i = 0; i < arr_size; i++)
     {
         if (arr[i] > 0)
@@ -115,6 +115,7 @@ int main()
     int test_array [6] = { 3, -1, -3, 0, 6, 4 };
 
     //-------------- Part A --------------//
+    std::cout << "Part A: ";
     int out_pos_arr_size1;
     int *new_array1 = get_pos_nums1(test_array, 6, out_pos_arr_size1);
 
@@ -122,24 +123,28 @@ int main()
     delete [] new_array1;
 
     //-------------- Part B --------------//
+    std::cout << "Part B: ";
     int out_pos_arr_size_ptr2;
-    int *new_array2 = get_pos_nums2(test_array, 6, &out_pos_arr_size_ptr2);
+    int *new_array2 = get_pos_nums2(test_array, 6, &out_pos_arr_size_ptr2); // & gets address of var
 
     print_array(new_array2, out_pos_arr_size_ptr2);
     delete [] new_array2;
 
     //-------------- Part C --------------//
-    int *new_array3 = nullptr;
+    std::cout << "Part C: ";
     int out_pos_arr_size3;
+    int *new_array3 = nullptr;
     get_pos_nums3(test_array, 6, new_array3, out_pos_arr_size3);
 
     print_array(new_array3, out_pos_arr_size3);
     delete [] new_array3;
 
     //-------------- Part D --------------//
-
+    std::cout << "Part D: ";
     int out_pos_arr_size4;
     int *new_array4 = nullptr;
+    // ** is pointer to pointer. So we declare *new_array4 to be a pointer, and get the address
+    // using &
     get_pos_nums4(test_array, 6, &new_array4, &out_pos_arr_size4);
 
     print_array(new_array4, out_pos_arr_size4);
