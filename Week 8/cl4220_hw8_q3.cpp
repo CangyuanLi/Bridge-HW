82325625e2ebf52fc344a29bfae437ae430cbf53
CangyuanLi <everest229@gmail.com>
2022-05-22 11:57:32 -0400

    q3

--- Week 8/cl4220_hw8_q3.cpp
+++ Week 8/cl4220_hw8_q3.cpp
@@ -1,7 +1,6 @@
 #include <algorithm>
 #include <iostream>
 
-{ 9, -2, 14, 12, 3, 6, 2, 1, -9, 15 };
 int min_in_array1(int arr [], int arr_size)
 {
     if (arr_size == 1)
@@ -24,16 +23,16 @@ int min_in_array1(int arr [], int arr_size)
 
 int min_in_array2(int arr [], int low, int high)
 {
-    if (arr_size == 1)
+    if (high == low)
     {
-        return arr[0];
+        return arr[low];
     }
     else
     {
-        int min_val = min_in_array1(arr, arr_size - 1);
-        if (arr[arr_size - 1] < min_val)
+        int min_val = min_in_array2(arr, low, high - 1);
+        if (arr[high] < min_val)
         {
-            return arr[arr_size - 1];
+            return arr[high];
         }
         else
         {
@@ -47,10 +46,9 @@ int main()
     int arr[10] = { 9, -2, 14, 12, 3, 6, 2, 1, -9, 15 };
     int res1, res2, res3, res4;
     res1 = min_in_array1(arr, 10);
-    std::cout << res1;
-    // res2 = min_in_array2(arr, 0, 9);
-    // std::cout << res1 << ' ' << res2 << std::endl; //should both be -9
-    // res3 = min_in_array2(arr, 2, 5);
-    // res4 = min_in_array1(arr + 2, 4); //arr+2 is equivalent to &(arr[2])
-    // std::cout << res3 << ' ' << res4 << std::endl; //should both be 
+    res2 = min_in_array2(arr, 0, 9);
+    std::cout << res1 << ' ' << res2 << std::endl; //should both be -9
+    res3 = min_in_array2(arr, 2, 5);
+    res4 = min_in_array1(arr + 2, 4); //arr+2 is equivalent to &(arr[2])
+    std::cout << res3 << ' ' << res4 << std::endl; //should both be 3
 }
